dfeabbe2c5a8970f153bc6340024215b8ad886cb
Nicolas Yuen <niyuen@microsoft.com>
2019-06-26 09:43:43 +0200

    adding feature preview script

---
+++ 00-preview-fearures/enablePreview.sh
@@ -0,0 +1,10 @@
+az extension add --name aks-preview
+az feature register -n WindowsPreview --namespace Microsoft.ContainerService
+az feature register -n AKSLockingDownEgressPreview --namespace Microsoft.ContainerService
+az feature register -n MultiAgentpoolPreview --namespace Microsoft.ContainerService
+az feature register -n APIServerSecurityPreview --namespace Microsoft.ContainerService
+az feature register -n VMSSPreview --namespace Microsoft.ContainerService
+az feature register --name AKSAuditLog --namespace Microsoft.ContainerService
+az feature register --name PodSecurityPolicyPreview --namespace Microsoft.ContainerService
+az provider register -n Microsoft.ContainerService
+
