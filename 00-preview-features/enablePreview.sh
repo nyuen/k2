5caa0c7bc8031745224bb02ed5bcdef00dd2aea0
Nicolas Yuen <niyuen@microsoft.com>
2019-06-26 10:02:37 +0200

    adding standard load balancer feature preview

--- 00-preview-features/enablePreview.sh
+++ 00-preview-features/enablePreview.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 az extension add --name aks-preview
 az feature register -n WindowsPreview --namespace Microsoft.ContainerService
 az feature register -n AKSLockingDownEgressPreview --namespace Microsoft.ContainerService
@@ -6,5 +8,6 @@ az feature register -n APIServerSecurityPreview --namespace Microsoft.ContainerS
 az feature register -n VMSSPreview --namespace Microsoft.ContainerService
 az feature register --name AKSAuditLog --namespace Microsoft.ContainerService
 az feature register --name PodSecurityPolicyPreview --namespace Microsoft.ContainerService
+az feature register --namespace "Microsoft.ContainerService" --name "AKSAzureStandardLoadBalancer"
 az provider register -n Microsoft.ContainerService
 
